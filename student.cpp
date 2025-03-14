0bf7ef3acd2666c49c52305b063ecc02ee09bd2c
ljimen1 <lucasjimenez15@developforgood.org>
2025-03-14 17:55:50 -0600

    Add constructor for Student class

--- student.cpp
+++ student.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
 #include "student.h"
 
-
+// Constructor
+Student::Student(std::string studentID, std::string firstName, std::string lastName, std::string emailAddress, int age, int daysInCourse1, int daysInCourse2, int daysInCourse3, DegreeProgram degreeProgram) {
+    this->studentID = studentID;
+    this->firstName = firstName;
+    this->lastName = lastName;
+    this->emailAddress = emailAddress;
+    this->age = age;
+    this->daysInCourse[0] = daysInCourse1;
+    this->daysInCourse[1] = daysInCourse2;
+    this->daysInCourse[2] = daysInCourse3;
+    this->degreeProgram = degreeProgram;
+}
 
 // Accessors
 std::string Student::getStudentID() { return studentID; }
