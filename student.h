a3d84d472a92127ec1131e644d06cbf7fcfd1b85
ljimen1 <lucasjimenez15@developforgood.org>
2025-03-14 17:50:58 -0600

    Add Student class skeleton in student.h

---
+++ student.h
@@ -0,0 +1,42 @@
+#ifndef STUDENT_H
+#define STUDENT_H
+
+#include <string>
+#include "degree.h"
+
+class Student {
+private:
+    std::string studentID;
+    std::string firstName;
+    std::string lastName;
+    std::string emailAddress;
+    int age;
+    int daysInCourse[3];
+    DegreeProgram degreeProgram;
+
+public:
+    Student(std::string studentID, std::string firstName, std::string lastName, std::string emailAddress, int age, int daysInCourse1, int daysInCourse2, int daysInCourse3, DegreeProgram degreeProgram);
+    ~Student();
+
+    // Accessors
+    std::string getStudentID();
+    std::string getFirstName();
+    std::string getLastName();
+    std::string getEmailAddress();
+    int getAge();
+    int* getDaysInCourse();
+    DegreeProgram getDegreeProgram();
+
+    // Mutators
+    void setStudentID(std::string studentID);
+    void setFirstName(std::string firstName);
+    void setLastName(std::string lastName);
+    void setEmailAddress(std::string emailAddress);
+    void setAge(int age);
+    void setDaysInCourse(int daysInCourse[]);
+    void setDegreeProgram(DegreeProgram degreeProgram);
+
+    void print();
+};
+
+#endif
