2a87796a84954c28d3451a13faef074a82df521b
ljimen1 <lucasjimenez15@developforgood.org>
2025-03-16 21:44:24 -0600

    Add main() function skeleton

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "roster.h"
+
+int main() {
+    std::cout << "Course Title: C867 Scripting and Programming - Applications" << std::endl;
+    std::cout << "Programming Language: C++" << std::endl;
+    std::cout << "WGU Student ID: [Your Student ID]" << std::endl;
+    std::cout << "Name: [Your Name]" << std::endl;
+
+    Roster classRoster;
+
+    
+
+    return 0;
+}
